e13a34f75fa52936bf322692ee04126138c2fe21
Hitesh Paryani <hitesh.paryani@justdial.com>
2020-07-20 00:47:58 +0530

    Updates

---
+++ precache-manifest.3e7f246ea6a4d92db2129e6c4069a5da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa84fe8bd706d5fac84282e5e4ec3fc1",
+    "url": "/snake-and-ladder/index.html"
+  },
+  {
+    "revision": "80e2fa20bc71eb755f04",
+    "url": "/snake-and-ladder/static/css/main.6a8b358b.chunk.css"
+  },
+  {
+    "revision": "dc1bc54f7f1b886aca1a",
+    "url": "/snake-and-ladder/static/js/2.92b1f9a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snake-and-ladder/static/js/2.92b1f9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e2fa20bc71eb755f04",
+    "url": "/snake-and-ladder/static/js/main.59cda72d.chunk.js"
+  },
+  {
+    "revision": "50c6855118f1a8501e71",
+    "url": "/snake-and-ladder/static/js/runtime-main.d87b745f.js"
+  },
+  {
+    "revision": "b44733eb8a5ab06e7e3a51274652614f",
+    "url": "/snake-and-ladder/static/media/dice.b44733eb.gif"
+  },
+  {
+    "revision": "8b161386e5e72f1dc1ad0260683ee662",
+    "url": "/snake-and-ladder/static/media/ladder3.8b161386.png"
+  },
+  {
+    "revision": "15cf8b5fae801732076bca41cdeab04e",
+    "url": "/snake-and-ladder/static/media/snake7.15cf8b5f.jpg"
+  }
+]);
